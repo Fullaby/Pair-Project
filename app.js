5ea28a87177fcb2716a6f8211902f5fdefc70821
steven-leonardo <stevenleonardolim@gmail.com>
2022-07-22 03:03:29 +0700

    commit 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 const routes = require('./routes')
 const path = require('path')
 const session= require('express-session');
